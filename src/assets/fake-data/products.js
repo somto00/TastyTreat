f5e814986b6595c96f998c75139452cae173269c
somto00 <onyekwelusomto29@gmail.com>
2025-06-20 00:29:35 +0100

    intial commit

---
+++ src/assets/fake-data/products.js
@@ -0,0 +1,93 @@
+
+import product_02_image_01 from "../images/product_2.1.jpg";
+import product_02_image_02 from "../images/product_2.2.jpg";
+import product_02_image_03 from "../images/product_2.3.jpg";
+
+import product_03_image_01 from "../images/product_3.1.jpg";
+import product_03_image_02 from "../images/product_3.2.jpg";
+import product_03_image_03 from "../images/product_3.3.jpg";
+
+import product_04_image_01 from "../images/product_4.1.jpg";
+import product_04_image_02 from "../images/product_4.2.jpg";
+import product_04_image_03 from "../images/product_4.3.png";
+
+
+const products = [
+
+
+  {
+    id: "02",
+    title: "Vegetarian Pizza",
+    price: 115.0,
+    image01: product_02_image_01,
+    image02: product_02_image_02,
+    image03: product_02_image_03,
+    category: "Pizza",
+    
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+  },
+
+  {
+    id: "03",
+    title: "Double Cheese Margherita",
+    price: 110.0,
+    image01: product_03_image_01,
+    image02: product_03_image_02,
+    image03: product_03_image_03,
+    category: "Pizza",
+    
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+  },
+
+  {
+    id: "04",
+    title: "Maxican Green Wave",
+    price: 110.0,
+    image01: product_04_image_01,
+    image02: product_04_image_02,
+    image03: product_04_image_03,
+    category: "Pizza",
+    
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+  },
+
+
+  {
+    id: "07",
+    title: "Seafood Pizza",
+    price: 115.0,
+    image01: product_02_image_02,
+    image02: product_02_image_01,
+    image03: product_02_image_03,
+    category: "Pizza",
+    
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+  },
+
+  {
+    id: "08",
+    title: "Thin Cheese Pizza",
+    price: 110.0,
+    image01: product_03_image_02,
+    image02: product_03_image_01,
+    image03: product_03_image_03,
+    category: "Pizza",
+    
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+  },
+
+  {
+    id: "09",
+    title: "Pizza With Mushroom",
+    price: 110.0,
+    image01: product_04_image_02,
+    image02: product_04_image_01,
+    image03: product_04_image_03,
+    category: "Pizza",
+    
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+  },
+
+];
+
+export default products;
